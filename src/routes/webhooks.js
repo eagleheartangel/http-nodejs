7bd8db1f28e08407cbceaae84a9814f915de6194
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 17:43:45 -0600

    second commit

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/verification-completed', (req, res) => {
-  console.log(req.body);
   res.render('index', { respuesta: JSON.stringify(req.body) });
 });
 
