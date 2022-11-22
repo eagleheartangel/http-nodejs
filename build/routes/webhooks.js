6eef60912c9bfd9c6518973301a95cb32f9cc4d1
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 17:45:47 -0600

    3

--- build/routes/webhooks.js
+++ build/routes/webhooks.js
@@ -3,7 +3,6 @@
 var _express = require("express");
 var router = (0, _express.Router)();
 router.get('/verification-completed', function (req, res) {
-  console.log(req.body);
   res.render('index', {
     respuesta: JSON.stringify(req.body)
   });
