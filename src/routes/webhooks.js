9f252270d36192dafba517f501a9e7a8e9a0580d
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 22:29:51 -0600

    14

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+  console.log(req.body);
   return res.render('index', { respuesta: JSON.stringify(req.body) });
 });
 
