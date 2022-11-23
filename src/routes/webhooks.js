084d9c97eb6bb74ebc5e21880a22c8b86b397b2a
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 20:54:40 -0600

    6

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/completed', (req, res) => {
-  const bdy = req.body;
-  console.log(bdy);
   return res.render('index', { respuesta: 'Hola mundo !!' });
 });
 
