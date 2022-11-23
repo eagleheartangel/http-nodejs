9e54420500d9a1ebfc194dde3829836313b5e0e9
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 22:06:56 -0600

    11

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const engine = require('ejs-mate');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
