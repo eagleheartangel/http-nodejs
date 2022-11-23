3902995b95fa4e83557482cbe610cc5acac1be57
eagleheartangel <eagleheartangel88@gmail.com>
2022-11-22 18:05:35 -0600

    4

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/verification-completed', (req, res) => {
-  res.render('index', { respuesta: JSON.stringify(req.body) });
+  res.render('index', { respuesta: 'Hola Ivette' });
 });
 
 module.exports = router;
